6440f307880da96d5546cf5ba87f4afa278a8e59
Jinrui Chen <jinrui@kooboo.cn>
2022-10-13 11:29:43 +0800

    添加 autoprefixer 插件补充css厂商前缀

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { normalizePath } from 'vite'
 import path from 'path'
+import autoprefixer from 'autoprefixer'
 
 // 全局 scss 文件的路径, 用 normalizePath 解决 window 下的路径问题
 const variablePath = normalizePath(path.resolve('./src/variable.scss'))
@@ -21,6 +22,15 @@ export default defineConfig({
         // additionalData 的内容会在每个 scss 文件的开头自动注入
         additionalData: `@import "${variablePath}";`,
       },
+    },
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
     },
   },
 })
