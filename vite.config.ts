879de984c09ea2c288b75d58e1e65d63c6eb6645
Jinrui Chen <jinrui@kooboo.cn>
2022-10-13 11:12:52 +0800

    vite with react & scss

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { normalizePath } from 'vite'
+import path from 'path'
+
+// 全局 scss 文件的路径, 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/variable.scss'))
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
+})
