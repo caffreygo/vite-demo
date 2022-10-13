4d6ed1ab1d17ac134114f6d58d13896a1abfe754
Jinrui Chen <jinrui@kooboo.cn>
2022-10-13 12:08:04 +0800

    windicss 支持 属性化 和 原子化

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,7 @@ import { devDependencies } from '../../../package.json'
 
 export function Main() {
   return (
-    <div className="p-20px text-center">
+    <div className="flex-c" border="2 rounded blue-200">
       <h1 className="font-bold text-2xl mb-2">vite version: {devDependencies.vite}</h1>
     </div>
   )
