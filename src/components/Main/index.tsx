6084905f7c600a9cfcb23d0288568a492977db0c
Jinrui Chen <jinrui@kooboo.cn>
2022-10-13 11:47:45 +0800

    支持 windicss
    1. vite.config.ts 引入 windi 插件
    2. 在src/main.tsx中引入一个必需的 import 语句:
    import "virtual:windi.css";

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import { devDependencies } from '../../../package.json'
+
+export function Main() {
+  return (
+    <div className="p-20px text-center">
+      <h1 className="font-bold text-2xl mb-2">vite version: {devDependencies.vite}</h1>
+    </div>
+  )
+}
